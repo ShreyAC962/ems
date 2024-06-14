815600653ecc189a03a9e2bec52c1820fce32fd9
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2024-06-14 15:28:51 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,65 +1,75 @@
 <?php
 
 session_start();
+include_once("includes/db.php");
 
-include("includes/db.php");
+if(!isset($_SESSION['admin_email'])){
+    echo "<script>windw.open('login.php','_self')</script>";
+}
 
-include("functions/functions.php");
+else{
 
 ?>
 
-<style>
-   
-body{
-    text-align:center;
-}
+<?php
 
-.custom-img {
-        width: 400px;
-        height: 400px;
-        object-fit: cover;
-}
+$admin_session = $_SESSION['admin_email'];
 
-.col-12{
-    background-image: url("https://images.unsplash.com/photo-1489513691500-41ef4acdb665?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGV2ZW50JTIwbWFuYWdlbWVudHxlbnwwfHwwfHx8MA%3D%3D");
-    background-size: cover; /* Ensures the image covers the entire area */
-    background-position: center; /* Centers the image */
-    height: 500px; /* Example height */
-    width: 100%; 
-    color: #ffffff;
-    padding: 150px 200px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
+$get_admin = "select * from admins where admin_email='$admin_session'";
 
-h1 {
-    margin: 0;
-    font-size: 2.5em;
-    text-align:center;
-}
+$run_admin = mysqli_query($con,$get_admin);
+    
+$row_admin = mysqli_fetch_array ($run_admin);
+    
+$admin_id = $row_admin ['admin_id'];
+    
+$admin_name = $row_admin ['admin_name'];
+    
+$admin_email = $row_admin ['admin_email'];
+    
+$admin_image = $row_admin ['admin_image'];
+    
+$admin_country = $row_admin ['admin_country'];
+    
+$admin_job = $row_admin ['admin_job'];
+    
+$admin_contact = $row_admin ['admin_contact'];
+    
+$admin_about = $row_admin['admin_about'];
+    
+$get_products = "select * from products ";
+    
+$run_products = mysqli_query ($con,$get_products);
 
-#current-time {
-    font-size: 1.2em;
-    margin-top: 10px;
-    text-align:center;
-}
+$count_products = mysqli_num_rows($run_products);
+    
+$get_customers = "select * from customers";
 
-main {
-    padding: 20px;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
+$run_customers = mysqli_query($con,$get_customers);
+    
+$count_customers = mysqli_num_rows($run_customers);
+    
+$get_p_categories= "select * from product_categories";
+    
+$run_p_categories = mysqli_query($con,$get_p_categories);
+    
+$count_p_categories = mysqli_num_rows($run_p_categories);
+ 
+$get_pending_orders = "select * from pending_orders";
+
+$run_pending_orders = mysqli_query($con,$get_pending_orders);
+ 
+$count_pending_orders = mysqli_num_rows($run_pending_orders);
 
 
-</style>
+?>
+
+
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-<title>Event Management System </title>
-
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet" >
@@ -69,261 +79,158 @@ main {
 <link href="styles/style.css" rel="stylesheet">
 
 <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-<div id="top"><!-- top Starts -->
-
-<div class="container"><!-- container Starts -->
-
-<div class="col-md-6 offer"><!-- col-md-6 offer Starts -->
-
-<a href="#" class="btn btn-info btn-sm" >
-<?php
-
-if(!isset($_SESSION['customer_email'])){
-
-echo "Welcome :Guest";
-
-
-}else{
-
-echo "Welcome : " . $_SESSION['customer_email'] . "";
-
-}
-
-
-?>
-</a>
-
-<a href="#">
-Total Price Of Events Booked :  <?php total_price(); ?> 
-</a>
-
-</div><!-- col-md-6 offer Ends -->
-
-<div class="col-md-6"><!-- col-md-6 Starts -->
-<ul class="menu"><!-- menu Starts -->
-
-<li>
-<a href="customer_register.php">
-Register
-</a>
-</li>
-
-<li>
-<?php
-
-if(!isset($_SESSION['customer_email'])){
-
-echo "<a href='checkout.php' >My Account</a>";
-
-}
-else{
-
-echo "<a href='customer/my_account.php?my_orders'>My Account</a>";
-
-}
-
-
-?>
-</li>
-
-<li>
-<a href="bookings.php">
- Go to Bookings
-</a>
-</li>
-
-<li>
-<?php
-
-if(!isset($_SESSION['customer_email'])){
-
-echo "<a href='checkout.php'> Login </a>";
-
-}else {
-
-echo "<a href='logout.php'> Logout </a>";
-
-}
-
-?>
-</li>
-
-</ul><!-- menu Ends -->
-
-</div><!-- col-md-6 Ends -->
-
-</div><!-- container Ends -->
-</div><!-- top Ends -->
-
-<div class="navbar navbar-default" id="navbar"><!-- navbar navbar-default Starts -->
-<div class="container" ><!-- container Starts -->
-
-<div class="navbar-header"><!-- navbar-header Starts -->
-
-<a class="navbar-brand home" href="index.php" ><!--- navbar navbar-brand home Starts -->
-
-
-
-</a><!--- navbar navbar-brand home Ends -->
-
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation"  >
-
-<span class="sr-only" >Toggle Navigation </span>
-
-<i class="fa fa-align-justify"></i>
-
-</button>
-
-
-
-</div><!-- navbar-header Ends -->
-
-<div class="navbar-collapse collapse" id="navigation" ><!-- navbar-collapse collapse Starts -->
-
-<div class="padding-nav" ><!-- padding-nav Starts -->
-
-<ul class="nav navbar-nav navbar-left"><!-- nav navbar-nav navbar-left Starts -->
-
-<li class="active">
-<a href="index.php"> Home </a>
-</li>
-
-<li>
-<a href="venues.php"> Events </a>
-</li>
-
-<li>
-<?php
-
-if(!isset($_SESSION['customer_email'])){
-
-echo "<a href='checkout.php' >My Account</a>";
-
-}
-else{
-
-echo "<a href='customer/my_account.php?my_orders'>My Account</a>";
-
-}
-
-
-?>
-</li>
-
-<li>
-<a href="bookings.php"> Bookings </a>
-</li>
-
-<li>
-<a href="contact.php"> Contact Us </a>
-</li>
-
-</ul><!-- nav navbar-nav navbar-left Ends -->
-
-</div><!-- padding-nav Ends -->
-
-<a class="btn btn-info navbar-btn right" href="bookings.php"><!-- btn btn-info navbar-btn right Starts -->
-
-
-
-<span>List Of Bookings </span>
-
-</a><!-- btn btn-info navbar-btn right Ends -->
-
-<div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Starts -->
-
-
-</div><!-- navbar-collapse collapse right Ends -->
-
-
-
-
-
-
-
-
-
-
-
-</div><!-- col-md-12 Ends -->
-
-</div><!-- container Ends -->
-
-<div class="row">
-    <div class="col-12">
-    <header>
-        <h1>Welcome to Our Event Management System</h1>
-        <h3>Quick and Hassle-Free Event Registration, Every Time.</h>
-        <div id="current-time"></div>
-    </header>
-    </div>
-</div?
-
-
-
-
-
-<div id="hot"><!-- hot Starts -->
-
-<div class="box"><!-- box Starts -->
-
-<div class="container"><!-- container Starts -->
-
-<div class="col-md-12"><!-- col-md-12 Starts -->
-
-<h2>Upcoming Events</h2>
-
-</div><!-- col-md-12 Ends -->
-
-</div><!-- container Ends -->
-
-</div><!-- box Ends -->
-
-</div><!-- hot Ends -->
-
-
-<div id="content" class="container"><!-- container Starts -->
-
-<div class="row"><!-- row Starts -->
-
-<?php
-
-getPro();
-
-?>
-
-</div><!-- row Ends -->
-
-</div><!-- container Ends -->
-
-<?php
-
-include("includes/footer.php");
-
-?>
-
 <script src="js/jquery.min.js"> </script>
 
 <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
 
-<script>
-function updateTime() {
-    const currentTimeElement = document.getElementById('current-time');
-    const now = new Date();
-    const formattedTime = now.toLocaleTimeString();
-    currentTimeElement.textContent = `Current Time: ${formattedTime}`;
-}
+<div id="wrapper"> <!--wrapper start-->
 
-document.addEventListener('DOMContentLoaded', () => {
-    updateTime();
-    setInterval(updateTime, 1000);
-});
-</script>
+<?php include_once("includes/sidebar.php")  ?>    
+            
+<div id="page-wrapper"><!--page-wrapper start-->
 
+<div class="container-fluid"> <!--container-fluid start-->
+ <?php
+    
+    if(isset($_GET['dashboard'])){
+    include("dashboard.php"); }
+    
+    if(isset($_GET['insert_product'])){
+        include_once("insert_product.php");
+    }
+        
+    if(isset($_GET['view_products'])){
+        include_once("view_products.php");
+    } 
+    
+    
+    if(isset($_GET['delete_product'])){
+        
+        include_once("delete_product.php");
+    }
+        
+    if(isset($_GET['edit_product'])){
+        include_once("edit_product.php");
+    }
+    
+    if(isset($_GET['insert_p_cat'])){
+
+      include("insert_p_cat.php"); }
+    
+    if(isset($_GET['view_p_cats'])){
+        include("view_p_cats.php");
+    }
+    
+    if(isset($_GET['delete_p_cat'])){
+        include("delete_p_cat.php");
+    }
+    
+    
+    if(isset($_GET['edit_p_cat'])){
+        include("edit_p_cat.php");
+    }
+    
+     if(isset($_GET['insert_cat'])){
+        include("insert_cat.php");
+    }
+    
+    if(isset($_GET['view_cats'])){
+        include("view_cats.php");
+    }
+    
+     if(isset($_GET['edit_cat'])){
+        include("edit_cat.php");
+    }
+    
+     if(isset($_GET['delete_cat'])){
+        include("delete_cat.php");
+    }
+    
+    if(isset($_GET['insert_slide'])){
+        include("insert_slide.php");
+    }
+    
+    if(isset($_GET['view_slides'])){
+        include("view_slides.php");
+    }
+    
+    if(isset($_GET['delete_slide'])){
+        include("delete_slide.php");
+    }
+    
+    if(isset($_GET['edit_slide'])){
+        include("edit_slide.php");
+    }
+    
+     if(isset($_GET['view_customers'])){
+        include("view_customers.php");
+    }
+    
+      if(isset($_GET['customer_delete'])){
+        include("customer_delete.php");
+    }
+    
+    
+      if(isset($_GET['view_orders'])){
+        include("view_orders.php");
+    }
+    
+       if(isset($_GET['order_delete'])){
+        include("order_delete.php");
+    }
+    
+    
+       if(isset($_GET['view_payments'])){
+        include("view_payments.php");
+    }
+    
+     if(isset($_GET['payment_delete'])){
+        include("payment_delete.php");
+    }
+    
+      if(isset($_GET['insert_user'])){
+        include("insert_user.php");
+    }
+    
+    
+    
+      if(isset($_GET['view_users'])){
+        include("view_users.php");
+    }
+    
+    
+      if(isset($_GET['user_delete'])){
+        include("user_delete.php");
+    }
+    
+     if(isset($_GET['user_profile'])){
+        include("user_profile.php");
+    }
+    
+    
+    
+    
+    
+    
+    
+    ?>   
+    
+</div>  <!--container-fluid start-->
+    
+    
+</div>  <!--page-wrapper start-->
+    
+</div> <!--wrapper stop-->
+            
 </body>
 </html>
+
+
+<?php
+     
+     }
+      
+     
+?>
